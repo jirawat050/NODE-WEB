f2b34849651c9fe1a8e199844bcf844a82dc48e9
jirawat050 <paoji@icloud.com>
2019-01-22 10:51:35 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const hbs = require('hbs') 
+const fs = require('fs') 
+var app = express();
+hbs.registerPartials(__dirname + '/views/patials');
+app.set('viewengine','hbs')
+app.use(express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+ var now = new Date().toString();
+ var log = `${now}: ${req.method} ${req.url}`;
+ console.log(log);
+ fs.appendFile('server.log', log +'\n',(err) =>{
+  if(err){
+      console.log('Unable')
+  }
+ });
+ next();
+});
+app.get('/',(req, res) => {
+    res.send({
+        name: 'Andrew',
+        likes: [
+            'Biking',
+            'Cities'
+        ]
+    });
+});
+app.get('/about',(req, res) => {
+    res.render('about.hbs',{
+        pageTitle: 'About Page',
+        CurrentYear: new Date().getFullYear()
+    });
+});
+app.get('/',(req, res) => {
+    res.send({
+        errMsg: 'Unable to handle request'
+    });
+});
+app.listen(8080);
