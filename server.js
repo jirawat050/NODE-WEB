c87da9b2d149d6f0835ec3d91a996cab5b919972
Jirawat <paoji@icloud.com>
2019-02-01 08:18:55 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var app = express();
 hbs.registerPartials(__dirname + '/views/patials');
 app.set('viewengine','hbs')
 app.use(express.static(__dirname + '/public'));
+
 app.use((req, res, next) => {
  var now = new Date().toString();
  var log = `${now}: ${req.method} ${req.url}`;
